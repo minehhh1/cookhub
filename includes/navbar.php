b8463a01f3971b59ea51343c0e3f5fe49547d6f4
minehhh <minehhhpro@gmail.com>
2025-05-19 19:19:51 +0200

    svg -> png 2.0

--- includes/navbar.php
+++ includes/navbar.php
@@ -15,7 +15,7 @@ if (session_status() === PHP_SESSION_NONE) {
 
     <!-- Logo -->
     <a class="navbar-brand" href="index.php">
-      <img src="../assets/cookhub.svg" alt="Logo" class="d-inline-block align-text-top">
+      <img src="../assets/cookhub.png" alt="Logo" class="d-inline-block align-text-top">
     </a>
 
     <!-- Navbar principale -->
